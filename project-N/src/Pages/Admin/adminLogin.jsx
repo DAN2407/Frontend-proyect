c973e35d90b6be2f4f5d7faa8d6391f6ea6b2ebc
DAN2407 <00042220@uca.edu.sv>
2024-05-08 12:17:23 -0600

    :tada: beginning of the project

---
+++ project-N/src/Pages/Admin/adminLogin.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+export const InicioAdmin = () => {
+  return (
+    <div className="relative w-[1440px] h-[1024px] bg-[#1c294b]">
+      <div className="flex w-[368px] h-[88px] items-center justify-center gap-[15px] p-[15px] absolute top-[827px] left-[521px] bg-white rounded-[10px] shadow-[0px_2px_3px_#0000002b,0px_0px_3px_#00000015]">
+        <img
+          className="relative w-[53.41px] h-[54.5px] mt-[-4.00px] ml-[-3.00px]"
+          alt="Google logo"
+          src="google-logo.svg"
+        />
+        <div className="relative w-fit [font-family:'Roboto-Medium',Helvetica] font-medium text-[#0000008a] text-[24px] tracking-[0] leading-[normal] whitespace-nowrap">
+          Registrarse con Google
+        </div>
+      </div>
+      <div className="absolute w-[447px] h-[74px] top-[149px] left-[497px] [font-family:'Inter-Bold',Helvetica] font-bold text-white text-[74px] tracking-[0] leading-[74px]">
+        Bienvenido
+      </div>
+      <img
+        className="absolute w-[491px] h-[533px] top-[230px] left-[460px] object-cover"
+        alt="Image"
+        src="image-4.png"
+      />
+    </div>
+  );
+};
---
+++ project-N/src/Pages/RN/login.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+export const LoginResidente = () => {
+  return (
+    <div className="relative w-[1440px] h-[1024px] bg-[#1c294b]">
+      <div className="absolute w-[645px] h-[706px] top-[159px] left-[398px]">
+        <div className="absolute top-0 left-[80px] [font-family:'Plus_Jakarta_Sans-Bold',Helvetica] font-bold text-white text-[96px] tracking-[0] leading-[normal]">
+          Bienvenido
+        </div>
+        <img className="absolute w-[645px] h-[533px] top-[106px] left-0 object-cover" alt="Image" src="image-4.png" />
+        <div className="flex w-[422px] h-[106px] items-center justify-center gap-[15px] p-[15px] absolute top-[600px] left-[111px] bg-white rounded-[50px] shadow-[0px_2px_3px_#0000002b,0px_0px_3px_#00000015]">
+          <img className="relative w-[26px] h-[26px]" alt="Google logo" src="google-logo.svg" />
+          <div className="relative w-fit [font-family:'Roboto-Medium',Helvetica] font-medium text-[#0000008a] text-[24px] tracking-[0] leading-[normal] whitespace-nowrap">
+            Registrarse con Google
+          </div>
+        </div>
+      </div>
+      <div className="absolute w-[1304px] h-[51px] top-[47px] left-[78px]">
+        <div className="absolute top-[12px] left-[1094px] [font-family:'Inter-Bold',Helvetica] font-bold text-white text-[32px] tracking-[0] leading-[normal] underline">
+          Iniciar sesión
+        </div>
+        <img className="absolute w-[57px] h-[51px] top-0 left-0" alt="Vector" src="vector.svg" />
+      </div>
+    </div>
+  );
+};
